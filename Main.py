8913fda2467a9548efe82992c55f2e00de82bfce
ngtienndungg <dungnt.works@gmail.com>
2023-05-21 00:58:53 +0700

    fruits and vegetables recognition and classification

--- Main.py
+++ Main.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="Magic world of machine learning",
+    page_icon="",
 )
 
-st.write("# Welcome to our final project !! 👋")
-
-st.sidebar.success("Select a demo above.")
+st.write("# Welcome to our final project - A magic world of machine learning 👋")
 
 st.markdown(
     """
